813f91f2384a14d6b4388a1a11a5d5cf51e0d080
Justin Tavares <justin@jtav.me>
2026-09-19 00:06:25 -0400

    bump ver in sidebar

--- webview/src/components/Sidebar.jsx
+++ webview/src/components/Sidebar.jsx
@@ -157,7 +157,7 @@ function Sidebar({ collapsed, backgroundUrl, onNavigate }) {
       <footer className="sidebar-footer">
         <hr className="sidebar-footer-divider" />
         <div className="sidebar-footer-row">
-          <span className="sidebar-footer-text">Justintube v2.2</span>
+          <span className="sidebar-footer-text">Justintube v2.3</span>
         </div>
         {user && previousLogIn && (
           <div className="sidebar-footer-row">
